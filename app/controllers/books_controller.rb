780be8c38ce42ab163ecb47394f3ed96e6126f20
MASHA <mashanikit@gmail.com>
2017-06-27 12:54:48 -0400

    added text and html index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,21 @@
 class BooksController < ApplicationController
 
+  def index
+    @books = Book.all
 
+  respond_to do |format|
+      format.html
+      format.text
+
+      format.json do
+        render json: @books.map { |book|
+          {
+            title: book.title, #full_name: (whatever you want in place of name, home & creepiness)
+            author: book.author, #can also say location:
+            already_read: book.already_read
+          }
+      }
+      end
+  end
+end
 end

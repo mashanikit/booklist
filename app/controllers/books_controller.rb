68368d2c24b5f2385fc001deb54bef307b3c1f5e
MASHA <mashanikit@gmail.com>
2017-06-27 13:22:36 -0400

    csv file

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,15 +7,21 @@ class BooksController < ApplicationController
       format.html
       format.text
 
+      format.csv do
+        render plain: @books.generate_csv(@books)
+
+
       format.json do
         render json: @books.map { |book|
           {
             title: book.title, #full_name: (whatever you want in place of name, home & creepiness)
+            book_id: book.id,
             author: book.author, #can also say location:
             already_read: book.already_read
           }
-      }
+        }
       end
+      end
+    end
   end
-end
 end
